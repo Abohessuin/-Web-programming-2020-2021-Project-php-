e2e68c8e8dae24a26e4ed1262fedd051f6a9cf25
Mohamed Hessuin <57597278+Abohessuin@users.noreply.github.com>
2021-01-13 19:19:14 +0200

    Add files via upload

---
+++ Po.js
@@ -0,0 +1,102 @@
+
+const LikeBnt = document.querySelectorAll(".Posts .fullpost .icons .fa-heart");
+const CommentBnt = document.querySelectorAll(".Posts .fullpost .icons .fa-comment");
+const Post = document.querySelectorAll(".Post");
+const Commentbox = document.querySelectorAll(".Post .caption .comment");
+const inputBnt = document.querySelectorAll(".Post .caption .comment input");
+const commentBnt = document.querySelectorAll(".Posts .fullpost .icons .fa-paper-plane");
+const textarea = document.querySelectorAll(".Post .caption .comment textarea");
+const Myname = document.querySelectorAll(".Post .profile .info .Name");
+const MessBox = document.querySelectorAll(".Post .profile .info .chat button");
+const chatbox= document.querySelectorAll(".Posts .fullpost .chatbox");
+const Send= document.querySelectorAll(".Posts .fullpost .chatbox i");
+const mess = document.querySelectorAll(".Posts .fullpost .chatbox .messagebox");
+const chat = document.querySelectorAll(".Posts .fullpost .chatbox .chats");
+
+Send.forEach((s, i) => {
+    s.addEventListener("click", () => {
+       var Input = mess[i].value;
+       const h1 = document.createElement("h1");
+       h1.innerText = Input;
+       chat[i].appendChild(h1);
+       mess[i].value="";
+    })
+});
+
+
+
+
+MessBox.forEach((mess, i) => {
+    mess.addEventListener("click", () => {
+
+        if (chatbox[i].style.height !== "30vh") {
+            chatbox[i].style.height = "30vh";
+            
+        } else {
+            
+            chatbox[i].style.height = "0vh";
+         
+        }
+
+
+
+
+
+    })
+});
+
+
+
+
+
+
+
+LikeBnt.forEach((Like, i) => {
+    Like.addEventListener("click", () => {
+
+        if (Like.style.background === "rgb(245, 71, 108) none repeat scroll 0% 0%") {
+
+            Like.style.background = "";
+            console.log(Like);
+        } else {
+
+            Like.style.background = "rgb(245, 71, 108)";
+        }
+
+    })
+});
+
+
+CommentBnt.forEach((comment, i) => {
+    comment.addEventListener("click", () => {
+
+        if (Post[i].style.height !== "85vh") {
+            Post[i].style.height = "85vh";
+            Commentbox[i].style.display = "inline";
+        } else {
+            console.log(4);
+            Post[i].style.height = "66vh";
+            Commentbox[i].style.display = "none";
+        }
+
+
+        if (comment.style.background === "rgb(245, 71, 108) none repeat scroll 0% 0%") {
+
+            comment.style.background = "";
+            
+        } else {
+
+            comment.style.background = "rgb(245, 71, 108)";
+        }
+
+
+    })
+});
+
+commentBnt.forEach((comment, i) => {
+    comment.addEventListener("click", () => {
+        
+        textarea[i].append(inputBnt[i].value + "\n");
+        inputBnt[i].value = "";
+    })
+});
--- style.php
+++ style.php
@@ -81,7 +81,7 @@
     background-color:white;
     text-shadow:1px 1px black;
 }
-.info{
+.inffo{
     margin-top:30px;
     margin-left:190px;
     display:flex;
@@ -131,13 +131,22 @@
    border:2px solid black;
    border-radius:10px;
    overflow:hidden; 
-   margin-top:28px
+   margin-top:30px;
+   justify-content:space-between;
 
 }
 
 .leftInfo{
-    width:40%;
+    width:30%;
+    margin-top:20px;
  
+}
+.rightInfo{
+    width:70%;
+    padding-left:20px;
+    margin-top:10px;
+    margin-left:30px;
+    
 }
 
 .userSkills{
@@ -186,4 +195,233 @@
 .skills{
     border:5px solid gray;
     padding:5px;
+}
+
+
+.Posts {
+background-color: #272a2f;
+width: 44vw;
+height: 100%;
+margin-top: 2%;
+border-radius: 1%;
+box-shadow: 0px 10px 10px rgba(245, 71, 108, 1);
+display: flex;
+flex-direction: column;
+}
+
+.Posts .fullpost {
+width: 100%;
+height: 100%;
+display: flex;
+position: relative;
+}
+.Posts .fullpost .chatbox {
+width: 10vw;
+height: 0vh;
+
+transition: width 2s;
+transition: all 1.5s ease-out;
+/*
+display:inline;
+height: 30vh;*/
+margin-left: 2%;
+margin-top: 5%;
+border: solid;
+border-width: 0.1px;
+border-color: rgb(245, 71, 108);
+border-radius: 2%;
+box-shadow: 0px 10px 10px rgba(245, 71, 108, 0.5);
+position: relative;
+}
+.Posts .fullpost .chatbox .chats {
+width: 100%;
+height: 88%;
+overflow: scroll;
+overflow-x: hidden;
+/*background-color: aliceblue;*/
+}
+.Posts .fullpost .chatbox .chats h1 {
+font-size: larger;
+color: aliceblue;
+/*background-color: aliceblue;*/
+}
+.Posts .fullpost .chatbox i {
+position: absolute;
+font-size: 35px;
+cursor: pointer;
+margin-top: 0%;
+right: 1%;
+color: rgb(224, 231, 235);
+/*background-color: aliceblue;*/
+}
+.Posts .fullpost .chatbox i:hover {
+color: rgb(167, 26, 57);
+/*background-color: aliceblue;*/
+}
+.Posts .fullpost .chatbox .messagebox {
+width: 100%;
+height: 2.05rem;
+margin-top: 0%;
+background: none;
+border: solid;
+border: none;
+border-top: solid rgb(245, 71, 108);
+border-width: 0.1px;
+font-family: "Cormorant Garamond", serif;
+color: aliceblue;
+/*background-color: aliceblue;*/
+}
+.Posts .fullpost .icons {
+display: flex;
+flex-direction: column;
+position: absolute;
+top: 55%;
+left: 87%;
+}
+
+.Posts .fullpost .icons i {
+font-size: 70px;
+margin-bottom: 60%;
+color: aliceblue;
+cursor: pointer;
+border-radius: 50%;
+background: #272a2f;
+box-shadow: 0px 10px 10px rgba(245, 71, 108, 1);
+}
+
+.Posts .fullpost .icons i:hover {
+background-color: rgb(167, 26, 57);
+}
+
+.Post {
+background-color: #272a2f;
+box-shadow: 0px 10px 10px rgba(245, 71, 108, 1);
+margin-top: 5%;
+margin-left: 5%;
+border-radius: 0.5%;
+transition: width 2s;
+transition: all 0.75s ease-out;
+border: solid rgb(245, 71, 108);
+border-width: 0.1px;
+width: 45vw;
+height: 66vh;
+/* height: 82vh; */
+}
+
+.Post .profile {
+display: flex;
+}
+
+.Post .profile img {
+flex: 0.1 1;
+width: 9%;
+border-radius: 10%;
+box-shadow: 0px 10px 10px rgba(245, 71, 108, 0.5);
+margin-top: 2%;
+margin-left: 2%;
+}
+
+.Post .profile .info {
+flex: 2 1;
+margin-top: 7%;
+margin-left: 2%;
+display: flex;
+justify-content: space-between;
+}
+.Post .profile .info .chat {
+display: flex;
+flex-direction: column;
+}
+.Post .profile .info .chat button {
+display: flex;
+flex-direction: column;
+padding: 0.5rem 1rem;
+margin-right: 1rem;
+cursor: pointer;
+border-color: rgb(245, 71, 108);
+border-radius: 5%;
+color: aliceblue;
+box-shadow: 0px 10px 10px rgba(245, 71, 108, 0.1);
+background: none;
+font-family: "Cormorant Garamond", serif;
+}
+.Post .profile .info .chat button:hover {
+background-color: rgb(245, 71, 108);
+color: aliceblue;
+}
+.Post .profile .info h1 {
+color: whitesmoke;
+font-family: "Cormorant Garamond", serif;
+font-size: x-large;
+}
+
+.Post .profile .info h2 {
+color: rgb(245, 71, 108);
+font-family: "Cormorant Garamond", serif;
+font-size: small;
+margin-right: 2%;
+}
+
+.Post .profile .info .Hr {
+font-size: smaller;
+color: rgb(245, 71, 108);
+font-family: "Courier New", Courier, monospace;
+}
+
+.Post .caption {
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+}
+
+.Post .caption .comment {
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+margin-top: 2%;
+width: 90%;
+
+transition: all 0.75s ease-out;
+/* displ inline */
+display: none;
+}
+
+.Post .caption .comment input {
+box-shadow: 0px 10px 10px rgba(245, 71, 108, 0.05);
+margin-top: 1%;
+padding: 1rem 2rem;
+width: 80%;
+border-radius: 1%;
+color: aliceblue;
+margin-left: 10%;
+font-family: "Cormorant Garamond", serif;
+background: #272a2f;
+}
+
+.Post .caption .comment textarea {
+box-shadow: 0px 10px 10px rgba(245, 71, 108, 0.05);
+margin-top: 1%;
+color: aliceblue;
+font-size: medium;
+width: 100%;
+border-radius: 1%;
+font-family: "Cormorant Garamond", serif;
+background: #272a2f;
+pointer-events: none;
+}
+
+.Post .caption p {
+font-family: "Cormorant Garamond", serif;
+font-size: x-large;
+color: whitesmoke;
+margin-top: 4%;
+margin-left: 2%;
+}
+
+.Post .caption .sea {
+margin-top: 1%;
+width: 30vw;
+box-shadow: 0px 10px 10px rgba(245, 71, 108, 0.2);
 }
