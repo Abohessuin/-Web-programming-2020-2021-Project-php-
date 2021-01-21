ffd596f6eb26e2cf0a15c121e901b3f488e27039
HusseinMohamed13 <58082880+HusseinMohamed13@users.noreply.github.com>
2021-01-21 23:20:47 +0200

    Fix bug

--- EditInfo.php
+++ EditInfo.php
@@ -2,71 +2,81 @@
 <html>
 
 <head>
-    <link rel="stylesheet" type="text/css" href="EditInfoStyle.php">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <script type="text/javascript" src="jquery-3.4.1.js"></script>
+  <link rel="stylesheet" type="text/css" href="EditInfoStyle.php">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <script type="text/javascript" src="jquery-3.4.1.js"></script>
 
-    <script type="text/javascript">
-    $(document).ready(function() {
+  <script type="text/javascript">
+   function Function1() {
+var fullPath = document.getElementById("imgPath").value;
+if (fullPath) {
+    var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+    var filename = fullPath.substring(startIndex);
+    if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+        filename = filename.substring(1);
+    }
+document.getElementById("image").src = filename;
+}
 
-    });
-    </script>
+}
+   
+  </script>
 </head>
 
 <body>
-    <div class="outer-container">
-        <div class="container">
-            <div class="title">
-                Edit Account Info
-            </div>
-            <hr>
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="text-center">
-                        <img src="//placehold.it/100" class="avatar img-circle" alt="avatar">
-                        <input type="file" class="form-control a">
-                    </div>
-                </div>
-                <div class="col-md-9 personal-info">
-                    <form class="form-horizontal" role="form">
-                        <div class="form-group">
-                            <label class="col-lg-3 control-label">First name:</label>
-                            <div class="col-lg-8">
-                                <input class="form-control" type="text" value="Hussein">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-lg-3 control-label">Last name:</label>
-                            <div class="col-lg-8">
-                                <input class="form-control" type="text" value="Mohamed">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-md-3 control-label">Password:</label>
-                            <div class="col-md-8">
-                                <input class="form-control" type="password" value="11111122333">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-md-3 control-label">Confirm password:</label>
-                            <div class="col-md-8">
-                                <input class="form-control" type="password" value="11111122333">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-md-3 control-label"></label>
-                            <div class="col-md-8">
-                                <input type="button" class="btn btn-primary" value="Save Changes">
-                                <span></span>
-                                <input type="reset" class="btn btn-default" value="Cancel">
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  <div class="outer-container">
+    <div class="container">
+      <div class="title">
+        Edit Account Info
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="text-center">
+            <img src="//placehold.it/100" class="avatar img-circle" id="image" alt="avatar">
+            <input type="file" class="form-control a"  id="imgPath" accept=".jpg,.jpeg,.png" onchange="Function1()">
+          </div>
         </div>
+        <div class="col-md-9 personal-info">
+          <form class="form-horizontal" role="form">
+            <div class="form-group">
+              <label class="col-lg-3 control-label">First name:</label>
+              <div class="col-lg-8">
+                <input class="form-control" type="text" value="Hussein">
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-lg-3 control-label">Last name:</label>
+              <div class="col-lg-8">
+                <input class="form-control" type="text" value="Mohamed">
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-md-3 control-label">Password:</label>
+              <div class="col-md-8">
+                <input class="form-control" type="password" value="11111122333">
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-md-3 control-label">Confirm password:</label>
+              <div class="col-md-8">
+                <input class="form-control" type="password" value="11111122333">
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-md-3 control-label"></label>
+              <div class="col-md-8">
+                <input type="button" class="btn btn-primary" value="Save Changes" id="bt1">
+                <span></span>
+                <input type="reset" class="btn btn-default" value="Cancel">
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
+  </div>
 </body>
 
 </html>
